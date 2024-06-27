9e66f776178e7b969b046c2aa8948b9797802d1e
eodaneze <ezealiokechukwu1999@gmail.com>
2024-06-27 02:38:20 -0700

    folder structure and app start

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-jccncbcb
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 9000;
+
+app.listen(PORT, () => {
+      console.log(`server is running on http://localhost:${PORT}`);
+})
